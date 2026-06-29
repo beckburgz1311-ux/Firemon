bbe5df0eaa9be1d42bc69b7ef4cb03bf502757d0
beckburgz1311-ux <beck.burgz1311@gmail.com>
2026-06-29 22:12:43 +0100

    Retire old Firemon offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,16 @@
-const CACHE="firemon-v4";
-const ASSETS=[
-  "./",
-  "./index.html",
-  "./style.css?v=4",
-  "./src/gzip-polyfill.js?v=4",
-  "./src/game.js?v=4",
-  "./manifest.webmanifest"
-];
+const CACHE = 'firemon-retired-v5';
 
-self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate",event=>{
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET")return;
-
-  if(event.request.mode==="navigate"){
-    event.respondWith(
-      fetch(event.request)
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put("./index.html",copy));
-          return response;
-        })
-        .catch(()=>caches.match("./index.html"))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(event.request))
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
