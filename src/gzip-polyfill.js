238b636cc2eba4655042f057bb02a490ec996ef2
beckburgz1311-ux <beck.burgz1311@gmail.com>
2026-06-29 22:07:16 +0100

    Add Safari gzip compatibility layer

---
+++ src/gzip-polyfill.js
@@ -0,0 +1,33 @@
+(() => {
+  "use strict";
+
+  if (!window.pako || !window.TransformStream) {
+    throw new Error("Firemon compatibility layer could not load.");
+  }
+
+  class FiremonDecompressionStream {
+    constructor(format) {
+      if (format !== "gzip") throw new TypeError("Only gzip is supported.");
+      const chunks = [];
+      const stream = new TransformStream({
+        transform(chunk) {
+          chunks.push(chunk instanceof Uint8Array ? chunk : new Uint8Array(chunk));
+        },
+        flush(controller) {
+          const length = chunks.reduce((total, chunk) => total + chunk.length, 0);
+          const input = new Uint8Array(length);
+          let offset = 0;
+          for (const chunk of chunks) {
+            input.set(chunk, offset);
+            offset += chunk.length;
+          }
+          controller.enqueue(window.pako.ungzip(input));
+        }
+      });
+      this.readable = stream.readable;
+      this.writable = stream.writable;
+    }
+  }
+
+  window.DecompressionStream = FiremonDecompressionStream;
+})();
